1f431346211a025a45c61c50d17e350926700f5a
MarkaGon <144377721+MarkaGon@users.noreply.github.com>
2024-02-15 22:07:12 -0500

    description of store

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
     <img src="logo2.ppj" alt= "SipAndSavor" width="58" />
 
-     <h1 style="color: #272932; background-color: #828489;"> Course Name:
+     <h1 style="color: #272932; background-color: #828489;">
      SipAndSavor </h1>
      <h2>Shipping</h2>
 </header>
--- home.php
+++ home.php
@@ -2,11 +2,26 @@
     <head>
         <title>SipAndSavor</title>
         <link rel="icon" href="logo2.png"  type="image/png"/>
+        <link rel="stylesheet" href="home.css"/>
         
     </head>
     <body>
+        <?php include ('header.php'); ?>
         <main>
-
+          <h3>Who are we?</h3>
+          <nav>
+            <ul>
+                <p>Founded in 2024 by a group of passionate tea and coffee lovers,
+                    SipAndSavor was created to share our love for the most decadent flavors
+                    of our favorite beverages. We aim to develop diverse tea and coffee accessories worldwide, providing our customers with 
+                    the tools to make perfect drinks. With a commitment to
+                    sustainability, quality, and ethically sourced manufacturing processes,
+                    we ensure a satisfactory experience with our accessories. Whether you are a 
+                    curious novice or a professional, we invite you to join the journey and experience
+                    our tea and coffee equipment so you can either brew or steep your favorite drinks.
+                </p>
+            </ul>
+          </nav>
         </main>
     </body>
 </html> 
