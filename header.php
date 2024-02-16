8136938cab082676e19f9bf4c0ece3e923011b4f
MarkaGon <144377721+MarkaGon@users.noreply.github.com>
2024-02-16 14:29:10 -0500

    fixxing

--- header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <img src="logo2.png" alt= "SipAndSavor" width="58" />
-
-     <h1 style="color: #e5e5e5; background-color:#58727f ;">
-     SipAndSavor </h1>
-     <a href="ship.php" target="_blank"><h3>Shipping</h3></a>
-     <br>
-</header>
