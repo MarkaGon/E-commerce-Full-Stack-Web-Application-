98e61d9473cef17962e65323308ed3f0e9dd10d3
MarkaGon <144377721+MarkaGon@users.noreply.github.com>
2024-02-15 21:33:27 -0500

    getting some files in the prj

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+    <img src="logo2.ppj" alt= "SipAndSavor" width="58" />
+
+     <h1 style="color: #272932; background-color: #828489;"> Course Name:
+     SipAndSavor </h1>
+     <h2>Shipping</h2>
+</header>
---
+++ home.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>SipAndSavor</title>
+        <link rel="icon" href="logo2.png"  type="image/png"/>
+        
+    </head>
+    <body>
+        <main>
+
+        </main>
+    </body>
+</html> 
