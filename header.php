bce7f3e374bac93c605f6595033cfd2504125d66
MarkaGon <144377721+MarkaGon@users.noreply.github.com>
2024-02-16 16:20:37 -0500

    home page done with proper heading and photos

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header>
     <img src="images/logo2.png" alt= "SipAndSavor Logo" width="58" />
-
     <h1 style="color: #e5e5e5; background-color:#58727f ;">
      SipAndSavor </h1>
+     <a href="home.php"target="_blank"><h2>Home</h2></a>
      <a href="ship.php"target="_blank"><h2>Shipping</h2></a>
 </header>
--- home.php
+++ home.php
@@ -17,9 +17,16 @@
                     sustainability, quality, and ethically sourced manufacturing processes,
                     we ensure a satisfactory experience with our accessories. Whether you are a 
                     curious novice or a professional, we invite you to join the journey and experience
-                    our tea and coffee equipment so you can either brew or steep your favorite drinks. </P>
+                    our tea and coffee equipment so you can either brew or steep your favorite drinks. 
+                </P> 
                 </ul>
             </nav>
+            <img class="carbon" src="images/cn.png" alt="Carbon Neutral"/>
         </main>
+        <h4>
+        <img class="kettle" src="images/eket.png" alt="Eletric Kettle"/>
+        <img class="press" src="images/fpress.jpg" alt="French Press"/>
+        <img class="grind" src="images/grinder.jpg" alt="grinder"/>
+        </h4>
     </body>
 </html>
