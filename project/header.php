501cb8e1ed9a18d91aa06f4fc46425afa7fd2bb0
MarkaGon <144377721+MarkaGon@users.noreply.github.com>
2024-02-16 14:56:22 -0500

    home page is set up and just need images

--- project/header.php
+++ project/header.php
@@ -1,7 +1,7 @@
 <header>
-    <img src="logo2.png" alt= "SipAndSavor Logo" width="58" />
+    <img src="images/logo2.png" alt= "SipAndSavor Logo" width="58" />
 
-    <h1 style="color: #83afa6; background-color:#d3a284 ;">
+    <h1 style="color: #e5e5e5; background-color:#58727f ;">
      SipAndSavor </h1>
      <a href="ship.php"target="_blank"><h2>Shipping</h2></a>
 </header>
