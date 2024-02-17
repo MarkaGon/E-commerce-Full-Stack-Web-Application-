6689709a3550f8767da57639c2fe1d22739cb49f
MarkaGon <144377721+MarkaGon@users.noreply.github.com>
2024-02-16 22:22:13 -0500

    ship page corrected final

--- ship.php
+++ ship.php
@@ -1,15 +1,17 @@
 <?php
-if (!isset($first_name)) { $first_name = ''; }
-if (!isset($last_name)) { $last_name = ''; }
-if (!isset($street_address)) { $street_address = ''; }
-if (!isset($city)) { $city = ''; }
-if (!isset($state)) { $state = ''; }
-if (!isset($zip_code)) { $zip_code = ''; }
-if (!isset($order_number)) { $order_number = ''; }
-if (!isset($dimensions)) { $dimensions = ''; }
-if (!isset($declared_value)) { $declared_value = ''; }
-?>
+if(!isset($first_name)) { $first_name = ''; }
+if(!isset($last_name)) { $last_name = ''; }
+if(!isset($address)) { $address = ''; }
+if(!isset($city)) { $city = ''; }
+if(!isset($state)) { $state = ''; }
+if(!isset($zipcode)) { $zipcode = ''; }
+if(!isset($order_number)) { $order_number = ''; }
+if(!isset($length)) { $length = ''; }
+if(!isset($width)) { $width = ''; }
+if(!isset($height)) { $height = ''; }
+if(!isset($declared_value)) { $declared_value = ''; }
 
+?>
 <html>
 <head>
     <title>Shipping Page</title>
@@ -23,29 +25,32 @@ if (!isset($declared_value)) { $declared_value = ''; }
         <p><?php echo htmlspecialchars($error_message); ?></p>
         <?php } ?>
         <label>First Name:</label>
-        <input type="text" name="first_name" pattern="[A-Za-z]+" title="Only letters are allowed" required value="<?php echo htmlspecialchars($first_name); ?>"><br>
+        <input type ="text" name = "first_name" value = "<?php echo htmlspecialchars($first_name); ?>"/><br>
         <label>Last Name:</label>
-        <input type="text" name="last_name" pattern="[A-Za-z]+" title="Only letters are allowed" required value="<?php echo htmlspecialchars($last_name); ?>"><br>
-        <label>Street Address:</label>
-        <input type="text" name="street_address" required value="<?php echo htmlspecialchars($street_address); ?>"><br>
+        <input type ="text" name = "last_name" value = "<?php echo htmlspecialchars($last_name); ?>"/> <br>
+        <label>Address:</label>
+        <input type ="text" name = "address" value = "<?php echo htmlspecialchars($address); ?>"/> <br>
         <label>City:</label>
-        <input type="text" name="city" pattern="[A-Za-z ]+" title="Only letters and spaces are allowed" required value="<?php echo htmlspecialchars($city); ?>"><br>
+        <input type ="text" name = "city" value = "<?php echo htmlspecialchars($city); ?>"/><br>
         <label>State:</label>
-        <input type="text" name="state" pattern="[A-Za-z]+" title="Only letters are allowed" required value="<?php echo htmlspecialchars($state); ?>"><br>
-        <label>Zip Code:</label>
-        <input type="text" name="zip_code" pattern="[0-9]+" title="Only numbers are allowed" required value="<?php echo htmlspecialchars($zip_code); ?>"><br>
-
+        <input type ="text" name = "state" value = "<?php echo htmlspecialchars($state); ?>"/> <br>
+        <label>Zipcode:</label>
+        <input type ="number" name = "zipcode" value = "<?php echo htmlspecialchars($zipcode); ?>"/> <br>
         <h3>Package Details</h3>
         <label>Order Number:</label>
-        <input type="text" name="order_number" pattern="[0-9]+" title="Only numbers are allowed" required value="<?php echo htmlspecialchars($order_number); ?>"><br>
-        <label>Dimensions (L x W x H):</label>
-        <input type="text" name="dimensions" pattern="[^*<>{}]+" title="No special characters are allowed except *{}<>" required value="<?php echo htmlspecialchars($dimensions); ?>"><br>
+        <input type ="number" name = "order_number" value = "<?php echo htmlspecialchars($order_number); ?>"/><br>
+        <label>Dimensions: Length</label>
+        <input type ="number" name = "length" value = "<?php echo htmlspecialchars($length); ?>"/> 
+        <label>Width</label>
+        <input type ="number" name = "width" value = "<?php echo htmlspecialchars($width); ?>"/> 
+        <label>Height</label>
+        <input type ="number" name = "height" value = "<?php echo htmlspecialchars($height); ?>"/> <br>
         <label>Declared Value:</label>
-        <input type="text" name="declared_value" pattern="[0-9]+" title="Only numbers are allowed" required value="<?php echo htmlspecialchars($declared_value); ?>"><br>
+        <input type ="number" name = "declared_value" value = "<?php echo htmlspecialchars($declared_value); ?>"/><br>
         <label>Ship Date:</label>
-        <input type="date" name="ship_date" required><br>
+        <input type ="date" name = "ship_date" value = "<?php echo htmlspecialchars($ship_date); ?>"/><br>
 
         <input type="submit" value="Submit">
-    </form>
+    </main>
 </body>
 </html>
