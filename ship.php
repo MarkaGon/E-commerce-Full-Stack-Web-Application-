cb3921242c53624ecb110f787b12c7f5d6b4e26e
MarkaGon <144377721+MarkaGon@users.noreply.github.com>
2024-02-16 23:16:05 -0500

    HALFWAY COMPLETE SHIPPING RESULTS

--- ship.php
+++ ship.php
@@ -5,6 +5,7 @@ if(!isset($address)) { $address = ''; }
 if(!isset($city)) { $city = ''; }
 if(!isset($state)) { $state = ''; }
 if(!isset($zipcode)) { $zipcode = ''; }
+if(!isset($ship_date)) { $ship_date = ''; }
 if(!isset($order_number)) { $order_number = ''; }
 if(!isset($length)) { $length = ''; }
 if(!isset($width)) { $width = ''; }
@@ -24,33 +25,42 @@ if(!isset($declared_value)) { $declared_value = ''; }
         <?php if (!empty($error_message)) { ?>
         <p><?php echo htmlspecialchars($error_message); ?></p>
         <?php } ?>
-        <label>First Name:</label>
-        <input type ="text" name = "first_name" value = "<?php echo htmlspecialchars($first_name); ?>"/><br>
-        <label>Last Name:</label>
-        <input type ="text" name = "last_name" value = "<?php echo htmlspecialchars($last_name); ?>"/> <br>
-        <label>Address:</label>
-        <input type ="text" name = "address" value = "<?php echo htmlspecialchars($address); ?>"/> <br>
-        <label>City:</label>
-        <input type ="text" name = "city" value = "<?php echo htmlspecialchars($city); ?>"/><br>
-        <label>State:</label>
-        <input type ="text" name = "state" value = "<?php echo htmlspecialchars($state); ?>"/> <br>
-        <label>Zipcode:</label>
-        <input type ="number" name = "zipcode" value = "<?php echo htmlspecialchars($zipcode); ?>"/> <br>
-        <h3>Package Details</h3>
-        <label>Order Number:</label>
-        <input type ="number" name = "order_number" value = "<?php echo htmlspecialchars($order_number); ?>"/><br>
-        <label>Dimensions: Length</label>
-        <input type ="number" name = "length" value = "<?php echo htmlspecialchars($length); ?>"/> 
-        <label>Width</label>
-        <input type ="number" name = "width" value = "<?php echo htmlspecialchars($width); ?>"/> 
-        <label>Height</label>
-        <input type ="number" name = "height" value = "<?php echo htmlspecialchars($height); ?>"/> <br>
-        <label>Declared Value:</label>
-        <input type ="number" name = "declared_value" value = "<?php echo htmlspecialchars($declared_value); ?>"/><br>
-        <label>Ship Date:</label>
-        <input type ="date" name = "ship_date" value = "<?php echo htmlspecialchars($ship_date); ?>"/><br>
-
-        <input type="submit" value="Submit">
+        <form action="ship_report.php" method="post">
+            <label>First Name:</label>
+            <input type ="text" name = "first_name" value = "<?php echo htmlspecialchars($first_name); ?>"/><br>
+            
+            <label>Last Name:</label>
+            <input type ="text" name = "last_name" value = "<?php echo htmlspecialchars($last_name); ?>"/> <br>
+            
+            <label>Address:</label>
+            <input type ="text" name = "address" value = "<?php echo htmlspecialchars($address); ?>"/> <br>
+            
+            <label>City:</label>
+            <input type ="text" name = "city" value = "<?php echo htmlspecialchars($city); ?>"/><br>
+            
+            <label>State:</label>
+            <input type ="text" name = "state" value = "<?php echo htmlspecialchars($state); ?>"/> <br>
+            
+            <label>Zipcode:</label>
+            <input type ="number" name = "zipcode" value = "<?php echo htmlspecialchars($zipcode); ?>"/> <br>
+            
+            <h3>Package Details</h3>
+            <label>Order Number:</label>
+            <input type ="number" name = "order_number" value = "<?php echo htmlspecialchars($order_number); ?>"/><br>
+            
+            <label>Dimensions: Length</label>
+            <input type ="number" name = "length" value = "<?php echo htmlspecialchars($length); ?>"/> 
+            <label>Width</label>
+            <input type ="number" name = "width" value = "<?php echo htmlspecialchars($width); ?>"/> 
+            <label>Height</label>
+            <input type ="number" name = "height" value = "<?php echo htmlspecialchars($height); ?>"/> <br>
+            
+            <label>Declared Value:</label>
+            <input type ="number" name = "declared_value" value = "<?php echo htmlspecialchars($declared_value); ?>"/><br>
+            <label>Ship Date:</label>
+            <input type ="date" name = "ship_date" value = "<?php echo htmlspecialchars($ship_date); ?>"/><br>
+            <input type="submit" value="Submit">
+        <form>
     </main>
 </body>
 </html>
---
+++ ship_report.php
@@ -0,0 +1,49 @@
+<?php
+echo '<link rel="stylesheet" href="home.css"/>';
+// Retrieve form data
+$first_name = filter_input(INPUT_POST, 'first_name');
+$last_name = filter_input(INPUT_POST, 'last_name');
+$address = filter_input(INPUT_POST, 'address');
+$city = filter_input(INPUT_POST, 'city');
+$state = filter_input(INPUT_POST, 'state');
+$zipcode = filter_input(INPUT_POST, 'zipcode', FILTER_VALIDATE_INT);
+$ship_date = filter_input(INPUT_POST, 'ship_date');
+$order_number = filter_input(INPUT_POST, 'order_number', FILTER_VALIDATE_INT);
+$length = filter_input(INPUT_POST, 'length', FILTER_VALIDATE_INT);
+$width = filter_input(INPUT_POST, 'width', FILTER_VALIDATE_INT);
+$height = filter_input(INPUT_POST, 'height', FILTER_VALIDATE_INT);
+$declared_value = filter_input(INPUT_POST, 'declared_value', FILTER_VALIDATE_INT);
+
+// Validate the dimensions
+if ($length > 36 || $width > 36 || $height > 36){
+    $error_message = "Error, No dimension can be larger than 36 inches";
+}
+
+// Make sure the value is not larger than $1000
+if ($declared_value >  1000) {
+    echo "Declared value can not be more than $1,000.";
+    exit;
+}
+
+
+
+
+// If validation passes, display the report
+
+// Display the report
+echo "<h2>Shipping Report</h2>";
+echo "From Address:  Coffee and Tea Ave<br>";
+echo "Recipient:  $first_name $last_name<br>";
+echo "To Address: $address, $city, $state, $zipcode<br>";
+echo "Package Dimensions:  $length x $width x $height <br>";
+echo "Declared Value: $$declared_value<br>";
+echo "Shipping Company:  UPS<br>";
+echo "Shipping Class:  UPS Premium<br>";
+echo "Tracking Number:   123456789<br>";
+echo "Order Number:  $order_number<br>";
+echo "Ship Date:  $ship_date<br>"; 
+?>
+<html>
+    <br><br>
+    <img src="images/bar.png" alt= "Barcode" width="290px" />
+</html>
